5ebbba9a2fc3b2036b086be2244f74d8418867e4
MRinshad <rinshadmorayur09@gmail.com>
2025-01-17 20:23:47 +0530

    new : added annexure to index

--- app/constants/filters.ts
+++ app/constants/filters.ts
@@ -1,4 +1,15 @@
 export const SEARCH_FIELDS = ['Title', 'Link', 'ISSN'];
 export const PUBLISHERS = ['Elsevier', 'Springer', 'Taylor & Francis', 'Wiley', 'Inderscience', 'World Scientific', 'Emerald', 'SAGE'];
-export const DATABASES = ['Scopus', 'SCI', 'SCIE', 'EI', 'ESCI', 'UGC', 'Pubmed/Medline', 'ABDC', 'NAAS'];
+export const DATABASES = [
+  'Scopus', 
+  'SCI', 
+  'SCIE', 
+  'EI', 
+  'ESCI', 
+  'UGC', 
+  'Pubmed/Medline', 
+  'ABDC', 
+  'NAAS',
+  'Annexure'  // Add ANNEXURE to the list
+];
 export const QUARTILES = ['Q1', 'Q2', 'Q3', 'Q4'];
